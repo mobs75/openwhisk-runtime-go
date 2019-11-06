69c336422067df701c500bb0d8be1a519bbd8604
Alessio Marinelli <a.marinelli@DEGHQ.LOCAL>
2019-11-06 17:29:42 +0100

    test preProcess-2

--- openwhisk/rootHandler.go
+++ openwhisk/rootHandler.go
@@ -20,7 +20,7 @@ type jsonString struct {
 	action_name    string `json:"action_name,omitempty"`
 	api_host       string `json:"api_host,omitempty"`
 	api_key        string `json:"api_key,omitempty"`
-	activation_id  string `json:"api_key,omitempty"`
+	activation_id  string `json:"activation_id,omitempty"`
 	transaction_id string `json:"transaction_id,omitempty"`
 	deadline       int    `json:"deadline,omitempty"`
 }
--- openwhisk/rootHandler_test.go
+++ openwhisk/rootHandler_test.go
@@ -1,7 +1,11 @@
 package openwhisk
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 )
 
@@ -31,4 +35,33 @@ func ExampleRootHandler() {
 	// Output: xxx
 
 	stopTestServer(ts, cur, log)
+}
+
+func ExamplePreprocess() {
+
+	var s1 jsonString
+
+	os.Setenv("value", `{"name": "TEST-name", "main": "TEST-main","code":"TEST-code","binary":"true","env": "{ "hello": "world", "hi": "all"}"}`)
+	os.Setenv("namespace", "TEST-namespace")
+	os.Setenv("action_name", "TEST-action_name")
+	os.Setenv("api_host", "TEST-action_name")
+	os.Setenv("api_key", "TEST-")
+	os.Setenv("activation_id", "TEST-api_key")
+	os.Setenv("transaction_id", "TEST-transaction_id")
+	os.Setenv("deadline", "9999")
+
+	var jsonData []byte
+
+	jsonData, err := json.Marshal(s1)
+
+	req, err := http.NewRequest("POST", "http://localhost:8088", bytes.NewBuffer(jsonData))
+	req.Header.Set("Content-Type", "application/json")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%s", jsonData)
+
+	// Output: xxx
 }
