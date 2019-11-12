410dd042461827d7197216ce0d3ec782c284e737
Michele Sciabarra <msciabarra@apache.org>
2019-11-12 16:09:29 +0100

    fixed

--- openwhisk/rootHandler_test.go
+++ openwhisk/rootHandler_test.go
@@ -2,7 +2,6 @@ package openwhisk
 
 import (
 	"bytes"
-	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
@@ -33,34 +32,19 @@ func ExampleRootHandler() {
 
 func ExamplePreprocess() {
 
-	jsonData := []byte(`{"name":"TEST-name","main":"TEST-main","code":"TEST-code","binary":"true","env":"{"hello":"world","hi":"all"}"}`)
+	os.Setenv("__OW_NAMESPACE", "__namespace__")
+	os.Setenv("__OW_ACTION_NAME", "__action_name__")
+	os.Setenv("__OW_API_HOST", "__api_host__")
+	os.Setenv("__OW_API_KEY", "__api_key__")
+	os.Setenv("__OW_ACTIVATION_ID", "__activation_id__")
+	os.Setenv("__OW_TRANSACTION_ID", "__transaction_id__")
+	os.Setenv("__OW_DEADLINE", "__deadline__")
 
-	os.Setenv("value", "JSON")
-	os.Setenv("namespace", "__OW_NAMESPACE")
-	os.Setenv("action_name", "__OW_ACTION_NAME")
-	os.Setenv("api_host", "__OW_API_HOST")
-	os.Setenv("api_key", "__OW_API_KEY")
-	os.Setenv("activation_id", "__OW_ACTIVATION_ID")
-	os.Setenv("transaction_id", "__OW_TRANSACTION_ID")
-	os.Setenv("deadline", "__OW_DEADLINE")
-
-	var aw actionWrapper
-	json.Unmarshal(jsonData, &aw)
-	fmt.Printf("%s", aw.Value)
-
-	url := "http://127.0.0.1:8888"
-	//r, err := http.NewRequest("POST", url, strings.NewReader(jsonData))
-	r, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
-	//r.Header.Set("Content-Type", "application/json")
-
-	if err != nil {
-		fmt.Println(err)
-	}
-	var jsonByte []byte = preProcess(r)
-	fmt.Printf("4")
-	fmt.Printf("%s", jsonByte)
-
+	data := bytes.NewBuffer([]byte(`{"hello":"world"}`))
+	r, _ := http.NewRequest("POST", "", data)
+	out, _ := preProcess(r)
+	fmt.Printf("%s", out)
 	// Output:
-	// xxx
+	// {"value":{"hello":"world"},"namespace":"__namespace__","action_name":"__action_name__","api_host":"__api_host__","api_key":"__api_key__","activation_id":"__activation_id__","transaction_id":"__transaction_id__"}
 
 }
